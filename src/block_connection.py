82e123a07203bfa5562a77eebbd897b5ea375315
WARAGON <63374544+wp1631@users.noreply.github.com>
2025-03-10 13:34:59 +0700

    add_block_conn simulation

--- src/block_connection.py
+++ src/block_connection.py
@@ -3,12 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.linalg import svd
+from sklearn.manifold import MDS
 
 MAX_BLOCK = 10
 ROW_NUM = 20
 COL_NUM = 5
 MODE = "row"
 mat_block = []
+
 for i in range(MAX_BLOCK):
     l_block_num = i
     r_block_num = (MAX_BLOCK - i) - 1
@@ -19,5 +22,18 @@ for i in range(MAX_BLOCK):
     mat_block.append(row)
 
 mat = np.block(mat_block)
-plt.imshow(mat)
+svd_data = svd(mat)
+svd_vec = svd_data[0]
+
+svd_vec /= np.sum(svd_vec, axis=1)
+emb = MDS(n_components=3)
+svd_vec_emb = emb.fit_transform(svd_vec)
+svd_vals = svd_data[1]
+
+plt.scatter(
+    svd_vec_emb[: svd_vals.shape[0], 0],
+    svd_vec_emb[: svd_vals.shape[0], 1],
+    c=svd_vals[: svd_vals.shape[0]],
+)
+plt.colorbar()
 plt.show()
