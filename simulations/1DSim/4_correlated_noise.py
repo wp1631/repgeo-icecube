b618ff3365948ec32266e97aae4d3e43ddc02322
wp1631 <waragonphoosuwan@gmail.com>
2025-07-13 17:40:20 +0700

    add bold center channel tuning line

--- simulations/1DSim/4_correlated_noise.py
+++ simulations/1DSim/4_correlated_noise.py
@@ -66,12 +66,13 @@ def plot_neural_orientation_tuning_profile(
     /,
     dpi: int = 200,
     plot_every: int = 300,
-    alpha: float = 0.3,
+    alpha: float = 0.2,
     cmap: str = "viridis",
 ):
     fig, ax = plt.subplots(dpi=dpi)
     probe_stim = np.sort(stimulus.orientation)
     num_lines = 1 + len(neuron_tuning_loc) // plot_every
+    _center = num_lines // 2
     _cmap = mpl.colormaps[cmap]
     _colors = _cmap(np.linspace(0, 1, num_lines))
     for tuning_loc, tuning_kappa, tuning_amp, col in zip(
@@ -86,6 +87,17 @@ def plot_neural_orientation_tuning_profile(
             alpha=alpha,
             c=col,
         )
+    ax.plot(
+        probe_stim,
+        neuron_tuning_amp[_center * plot_every]
+        * vonmises.pdf(
+            probe_stim,
+            loc=neuron_tuning_loc[_center * plot_every],
+            kappa=neuron_tuning_kappa[_center * plot_every],
+        ),
+        alpha=1,
+        c=_colors[_center],
+    )
     plt.title("Neural Tuning Function")
     plt.xlabel("Orientation")
     plt.xticks(
@@ -111,8 +123,18 @@ deriv_normed = np.linalg.norm(deriv, axis=1)
 fisher_info = deriv_normed / np.sqrt(NR_NUM)
 
 
-def plot_orientation_fisher_information():
-    plt.scatter(stimulus.orientation, fisher_info, c=stimulus.spatial_loc)
+def plot_orientation_fisher_information(
+    stimulus: Stimulus1D,
+    fisher_info: np.ndarray,
+    colored_by_spatial_loc: bool = True,
+    cmap: str = "inferno",
+):
+    if colored_by_spatial_loc:
+        plt.scatter(
+            stimulus.orientation, fisher_info, c=stimulus.spatial_loc, cmap=cmap
+        )
+    else:
+        plt.scatter(stimulus.orientation, fisher_info)
     plt.title("Fisher Information $J(\\theta)$")
     plt.xlabel("Orientation")
     plt.xticks(
@@ -123,33 +145,74 @@ def plot_orientation_fisher_information():
     plt.show()
 
 
-plot_orientation_fisher_information()
-#
+plot_orientation_fisher_information(stimulus, fisher_info)
 
-embedding = MDS(n_components=3, n_init=1)
-response_transformed_3d = embedding.fit_transform(neural_responses)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.scatter(
-    response_transformed_3d[:, 0],
-    response_transformed_3d[:, 1],
-    response_transformed_3d[:, 2],
-    c=stimulus_ori,
-    alpha=0.3,
-    cmap="hsv",
-)
 
-ax.set_xlabel("Dimension 1")
-ax.set_ylabel("Dimension 2")
-ax.set_zlabel("Dimension 3")
-plt.title("MDS Embedding of Sparse Orientation Coding, Neural Response MDS (3D)")
-plt.show()
+def plot_mds(
+    data: np.ndarray,
+    dim: int = 3,
+    alpha: float = 0.3,
+    cmap: str = "hsv",
+    /,
+    xlabel: str = "Dimension 1",
+    ylabel: str = "Dimension 2",
+    zlabel: str = "Dimension 3",
+    title: str = "MDS Embedding of the neural responses (3D)",
+):
+    embedding = MDS(n_components=dim)
+    _transformed_3d = embedding.fit_transform(data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    if dim == 2:
+        ax.scatter(
+            _transformed_3d[:, 0],
+            _transformed_3d[:, 1],
+            c=stimulus_ori,
+            alpha=alpha,
+            cmap=cmap,
+        )
+    if dim == 3:
+        ax.scatter(
+            _transformed_3d[:, 0],
+            _transformed_3d[:, 1],
+            _transformed_3d[:, 2],
+            c=stimulus_ori,
+            alpha=alpha,
+            cmap=cmap,
+        )
+    else:
+        raise NotImplementedError(
+            "Dimension for plot is not correct; need to be 2 or 3"
+        )
 
-neural_responses_sorted = neural_responses[np.argsort(stimulus_ori)]
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if dim == 3:
+        ax.set_zlabel(zlabel)
+    plt.title(title)
+    plt.show()
 
-p_dist = pdist(neural_responses_sorted)
-dist_mat = squareform(p_dist)
 
-plt.imshow(dist_mat)
-plt.colorbar()
-plt.show()
+# Plot MDS of the neural responses
+plot_mds(neural_responses)
+
+# Plot MDS of the orientation gradient
+plot_mds(deriv, title="MDS Embedding of the Gradient (3D)")
+
+# TODO Plot MDS of the location gradient
+# plot_mds()
+sort_index = np.argsort(stimulus_ori)
+
+
+def plot_RDM(neural_responses: np.ndarray, sort_index: np.ndarray):
+    neural_responses_sorted = neural_responses[sort_index]
+
+    p_dist = pdist(neural_responses_sorted)
+    dist_mat = squareform(p_dist)
+
+    plt.imshow(dist_mat)
+    plt.colorbar()
+    plt.show()
+
+
+plot_RDM(neural_responses, sort_index)
